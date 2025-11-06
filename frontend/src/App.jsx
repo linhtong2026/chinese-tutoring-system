6bb1ffd1e625ddcb635658998c75df1d249f78b6
ntna141 <ntna102@gmail.com>
2025-11-05 22:39:50 -0500

    setup clerk and authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/clerk-react'
+
 function App() {
   return (
     <div>
       <h1>Chinese Tutoring System</h1>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   )
 }
